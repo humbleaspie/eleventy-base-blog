2bc27dd855f064ae1499b9c2eba1f5f6348b1e0b
Elliot Davies <elliotdavies@Elliots-iMac.localdomain>
2021-11-04 14:15:46 +0000

    Updated 11ty.js
    Signed-off-by: Elliot Davies <elliotdavies@Elliots-iMac.localdomain>

--- .eleventy.js
+++ .eleventy.js
@@ -61,8 +61,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("assets");
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
